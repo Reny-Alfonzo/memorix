683f2cffa32728a293c39650d23ceead87b35ce6
¨Reny-Alfonzo¨ <renyalfonzo21@gmail.com>
2025-05-11 22:58:51 -0500

    solucionando problemas de desbordamiento de contenedores con el framework Bulma

--- memorix/forms.py
+++ memorix/forms.py
@@ -1,15 +1,19 @@
 from django import forms
 from .models import Tarjeta, Mazo
 
-
-
 class MazoForm(forms.ModelForm):
     class Meta:
         model = Mazo
         fields = ["nombre"]
+        widgets = {
+            "nombre": forms.TextInput(attrs={"class": "input", "placeholder": "Ingrese el nombre del mazo"}),
+        }
 
 class TarjetaForm(forms.ModelForm):
     class Meta:
         model = Tarjeta
-        fields = ["nombre", "mazo", "anverso", "reverso"]
-
+        fields = ["anverso", "reverso"]
+        widgets = {
+            "anverso": forms.TextInput(attrs={"class": "input", "placeholder": "Ingrese el anverso"}),
+            "reverso": forms.TextInput(attrs={"class": "input", "placeholder": "Ingrese el reverso"}),
+        }
--- memorix/views.py
+++ memorix/views.py
@@ -5,15 +5,6 @@ from .forms import MazoForm, TarjetaForm
 def home(request):
     return render(request, "memorix/home.html")
 
-def mis_mazos(request):
-    mazos = Mazo.objects.prefetch_related("tarjetas").all()  # Mejora el rendimiento
-    return render(request, "memorix/mis_mazos.html", {"mazos": mazos})
-
-def mis_tarjetas(request, mazo_id):
-    mazo = Mazo.objects.select_related().get(id=mazo_id)
-    tarjetas = mazo.tarjetas.all()
-    return render(request, "memorix/mis_tarjetas.html", {"mazo": mazo, "tarjetas": tarjetas})
-
 def crear_mazo(request):
     if request.method == "POST":
         form = MazoForm(request.POST)
@@ -24,8 +15,18 @@ def crear_mazo(request):
         form = MazoForm()
     return render(request, "memorix/crear_mazo.html", {"form": form})
 
+def mis_mazos(request):
+    mazos = Mazo.objects.prefetch_related("tarjetas").all()  # Mejora el rendimiento
+    return render(request, "memorix/mis_mazos.html", {"mazos": mazos})
+
+def mis_tarjetas(request, mazo_id):
+    mazo = Mazo.objects.select_related().get(id=mazo_id)
+    tarjetas = mazo.tarjetas.all()
+    return render(request, "memorix/mis_tarjetas.html", {"mazo": mazo, "tarjetas": tarjetas})
+
 def agregar_tarjeta(request, mazo_id):
     mazo = Mazo.objects.get(id=mazo_id)
+
     if request.method == "POST":
         form = TarjetaForm(request.POST)
         if form.is_valid():
@@ -37,3 +38,4 @@ def agregar_tarjeta(request, mazo_id):
         form = TarjetaForm()
 
     return render(request, "memorix/agregar_tarjeta.html", {"form": form, "mazo": mazo})
+
